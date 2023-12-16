73ecc57b061d68ceaa15c02d5f6d4c1ef74cf59f
itzy-suuport <itzygallerysu@gmail.com>
2023-12-16 19:55:23 +0900

    베이스경로 수정

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/yuna-message-book/",
   plugins: [
     vue({
       template: { transformAssetUrls },
